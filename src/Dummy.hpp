11e2096dee297d627aa531e3be11daa2af0e37db
Sylvain <sylvain.joyeux@tidewise.io>
2019-09-19 09:46:49 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace nmea2000
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace nmea2000
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <nmea2000/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    nmea2000::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
