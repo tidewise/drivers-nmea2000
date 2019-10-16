8544b58fa89d0c72c6e2fc90d11ebc06fbf8caf9
Sylvain <sylvain.joyeux@tidewise.io>
2019-10-16 15:11:56 -0300

    fix N2k reception using the Actisense USB adapter
    This adds support for escapes, whose importance was not prominent
    in one codebase I found, but turned out to be very important indeed.
    canboat (https://github.com/canboat/canboat/) contained the right
    logic.

--- src/ActisenseDriver.cpp
+++ src/ActisenseDriver.cpp
@@ -7,59 +7,83 @@ ActisenseDriver::ActisenseDriver()
 }
 
 int ActisenseDriver::extractPacket(uint8_t const* buffer, size_t buffer_size) const {
-    if (buffer_size < PRELUDE_SIZE) {
+    if (buffer_size < 3) {
         return 0;
     }
-
-    if (buffer[0] != ESCAPE ||
-        buffer[1] != START_OF_TEXT ||
-        buffer[2] != N2K_DATA_RECEIVED) {
+    else if (buffer[0] != ESCAPE || buffer[1] != START_OF_TEXT) {
+        return -1;
+    }
+    else if (buffer[2] != N2K_MSG_RECEIVED) {
         return -1;
     }
 
-    uint8_t message_length = buffer[3];
-    if (message_length < HEADER_SIZE - PRELUDE_SIZE + 1) {
-        return -1;
+    for (size_t i = 2; i < buffer_size - 1; ++i) {
+        if (buffer[i] == ESCAPE && buffer[i + 1] == START_OF_TEXT) {
+            return -i;
+        }
+        if (buffer[i] == ESCAPE && buffer[i + 1] == END_OF_TEXT) {
+            return validateFramedPacket(buffer, i + 2);
+        }
+
     }
 
-    size_t epilog_index = PRELUDE_SIZE + message_length;
-    if (buffer_size < epilog_index + EPILOG_SIZE) {
-        return 0;
-    }
-    else if (buffer[epilog_index] != ESCAPE ||
-             buffer[epilog_index + 1] != END_OF_TEXT) {
-        return -1;
-    }
+    return 0;
+}
 
-    uint8_t payload_length = buffer[14];
-    if (payload_length != message_length - 12) {
-        return -1; // two lengths do not agree
-    }
+int ActisenseDriver::validateFramedPacket(uint8_t const* buffer, size_t length) const {
+    // First validate the message length. We need to count the actual payload
+    // bytes to compute the CRC anyways
+    size_t count = 0;
+    uint8_t crc = 0;
+    for (size_t i = 2; i < length - EPILOGUE_SIZE; ++i, ++count) {
+        uint8_t b = buffer[i];
+        crc += b;
+        if (b == ESCAPE) {
+            ++i;
+        }
 
-    int sum = 0;
-    for (size_t i = PRELUDE_SIZE; i < epilog_index; ++i) {
-        sum += buffer[i];
+        if (count == 12) {
+            if (b + 11 != buffer[3]) {
+                return -length;
+            }
+        }
     }
-    if ((sum & 0xFF) != 0) { // invalid CRC
-        return -1;
-    }
+    count -= 2; // counted the command and length bytes
 
-    return PRELUDE_SIZE + message_length + EPILOG_SIZE;
+    if (((crc + buffer[length - EPILOGUE_SIZE]) & 0xFF) != 0) {
+        return -length;
+    }
+    else if (count != buffer[3]) {
+        return -length;
+    }
+    return length;
 }
 
 Message ActisenseDriver::readMessage() {
     uint8_t buffer[INTERNAL_BUFFER_SIZE];
-    readPacket(buffer, INTERNAL_BUFFER_SIZE);
+    size_t length = readPacket(buffer, INTERNAL_BUFFER_SIZE);
+
+    size_t out = 0;
+    // Decode the ESCAPE + ESCAPE patterns once and for all
+    // Only keep the "useful" parts
+    for (size_t in = 2; in < length - 3; ++in, ++out) {
+        uint8_t b = buffer[in];
+        buffer[out] = b;
+        if (b == ESCAPE) {
+            ++in;
+        }
+    }
 
     Message message;
     message.time = base::Time::now();
-    message.priority = buffer[4];
-    message.pgn = buffer[5] |
-                  static_cast<uint32_t>(buffer[6]) << 8 |
-                  static_cast<uint32_t>(buffer[7]) << 16;
-    message.destination = buffer[8];
-    message.source = buffer[9];
-    message.size = buffer[14];
-    std::copy(buffer + 15, buffer + 15 + message.size, message.payload);
+    message.priority = buffer[2];
+    message.pgn = buffer[3] |
+                  static_cast<uint32_t>(buffer[4]) << 8 |
+                  static_cast<uint32_t>(buffer[5]) << 16;
+    message.destination = buffer[6];
+    message.source = buffer[7];
+    message.size = buffer[12];
+    std::copy(buffer + 13, buffer + 13 + message.size, message.payload);
+
     return message;
 }
--- src/ActisenseDriver.hpp
+++ src/ActisenseDriver.hpp
@@ -14,17 +14,21 @@ namespace nmea2000 {
         static const int INTERNAL_BUFFER_SIZE = MAX_PACKET_SIZE * 4;
 
     private:
-        static const uint8_t PRELUDE_SIZE = 4;
-        static const uint8_t EPILOG_SIZE = 2;
+        static const uint8_t PROLOGUE_SIZE = 4;
+        static const uint8_t EPILOGUE_SIZE = 3;
         static const uint8_t HEADER_SIZE = 15;
         static const uint8_t ESCAPE = 0x10;
         static const uint8_t START_OF_TEXT = 0x02;
         static const uint8_t END_OF_TEXT = 0x03;
-        static const uint8_t N2K_DATA_RECEIVED = 0x93;
-        static const uint8_t BEM_CMD_RECEIVED = 0xA0;
+
+        static const uint8_t N2K_MSG_RECEIVED = 0x93;
+        static const uint8_t N2K_MSG_SEND = 0x94;
+        static const uint8_t ACTISENSE_CMD_RECEIVED = 0xA0;
+        static const uint8_t ACTISENSE_CMD_SEND = 0xA1;
 
     protected:
         virtual int extractPacket(uint8_t const* buffer, size_t buffer_size) const;
+        int validateFramedPacket(uint8_t const* buffer, size_t length) const;
 
     public:
         ActisenseDriver();
