8e25525bccada4952725e60072666da1dae0df80
Wellington Castro <wellington.castro@tidewise.io>
2026-06-02 19:01:34 -0300

    chore: use smart pointer to manage drivers

--- src/Adapters.cpp
+++ src/Adapters.cpp
@@ -1,7 +1,6 @@
-#include <nmea2000/Adapters.hpp>
-#include <nmea2000/ActisenseDriver.hpp>
 #include <canbus/Driver.hpp>
 #include <iostream>
+#include <nmea2000/Adapters.hpp>
 
 using namespace base;
 using namespace nmea2000::adapters;
@@ -33,13 +32,12 @@ CAN::CAN(std::string const& name, std::string const& type)
 CAN::CAN(std::string const& name, PGNLibrary const& library, std::string const& type)
     : m_driver{canbus::openCanDevice(name, type)}
     , m_library{library}
-    , m_receiver(m_library)
+    , m_receiver{m_library}
 {
 }
 
 CAN::~CAN() {
     m_driver->close();
-    delete m_driver;
 }
 
 void CAN::writeMessage(Message const& message) {
@@ -96,7 +94,6 @@ Actisense::Actisense(std::string const& uri)
 
 Actisense::~Actisense() {
     m_driver->close();
-    delete m_driver;
 }
 
 void Actisense::writeMessage(Message const& message) {
