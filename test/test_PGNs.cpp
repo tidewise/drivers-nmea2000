1948c0cd8afda0b66a4a3ce4fcbe278cf51e8a25
Jhonas Moura <jhonas.moura@tidewise.io>
2026-08-26 17:36:17 -0300

    chore: add unit test for AIS parsing from real ais data

--- test/test_PGNs.cpp
+++ test/test_PGNs.cpp
@@ -283,8 +283,297 @@ TEST_F(PGNsTest, it_parses_non_zero_terminated_ASCII_fields)
     ASSERT_EQ("abcd", parsed.local_datum);
 }
 
-template<typename M>
-M parse_message(vector<uint8_t> const& payload, int size, base::Time const& time) {
+TEST_F(PGNsTest, it_parses_AISClassAPositionReport)
+{
+    auto parsed = parse_message<AISClassAPositionReport>({
+        1, // payload[0]: msg_id (1), repeat_indicator (0)
+        189,
+        216,
+        87,
+        42, // payload[1..4]: user_id (710400189)
+        187,
+        62,
+        21,
+        227, // payload[5..8]: longitude -> -48.51469421
+        158,
+        173,
+        204,
+        240, // payload[9..12]: latitude -> -25.50216674
+        123, // payload[13]: pos_accuracy(1), raim(1), time_stamp(30)
+        255,
+        255, // payload[14..15]: cog -> 375.4878845
+        5,
+        0, // payload[16..17]: sog (5)
+        126,
+        132,
+        0, // payload[18..20]: communication state(skipped), ais transceiver
+           // information(0)
+        255,
+        255, // payload[21..22]: heading -> 375.4878845
+        255,
+        127, // payload[23..24]: rate of turn -> 187.7410736084
+        192, // payload[25]: nav status (192)
+        248, // payload[26]: reserved for regional (248)
+        255, // payload[27]: spare(255)
+    });
+
+    ASSERT_EQ(1, parsed.message_id);
+    ASSERT_EQ(0, parsed.repeat_indicator);
+    ASSERT_EQ(710400189, parsed.user_id);
+    ASSERT_NEAR(-48.51469421, parsed.longitude, 1e-6);
+    ASSERT_NEAR(-25.50216674, parsed.latitude, 1e-6);
+    ASSERT_EQ(1, parsed.position_accuracy);
+    ASSERT_EQ(1, parsed.raim);
+    ASSERT_EQ(30, parsed.time_stamp);
+    ASSERT_NEAR(375.4878845, parsed.cog, 1e-6);
+    ASSERT_NEAR(0.05, parsed.sog, 1e-6);
+    ASSERT_EQ(0, parsed.ais_transceiver_information);
+    ASSERT_NEAR(375.4878845, parsed.heading, 1e-6);
+    ASSERT_NEAR(187.7410736084, parsed.rate_of_turn, 1e-6);
+    ASSERT_EQ(192, parsed.nav_status);
+}
+
+TEST_F(PGNsTest, it_parses_AISClassBPositionReport)
+{
+    auto parsed = parse_message<AISClassBPositionReport>({
+        18, // payload[0]: msg_id (18), repeat_indicator (0)
+        164,
+        219,
+        87,
+        42, // payload[1..4]: user_id (710400932)
+        205,
+        5,
+        24,
+        227, // payload[5..8]: longitude -> -48.4964904785
+        0,
+        217,
+        202,
+        240, // payload[9..12]: latitude -> -25.51416397094
+        124, // payload[13]: pos_accuracy(0), raim(0), time_stamp(31)
+        255,
+        255, // payload[14..15]: cog (375.4878845) -> not available
+        0,
+        0, // payload[16..17]: sog (0)
+        0,
+        0,  // payload[18..19]: unused
+        32, // payload[20]: ais_transceiver_information (4)
+        255,
+        255, // payload[21..22]: heading (375.4878845) -> not available
+        0,   // payload[23]: regional application (0)
+        112, // payload[24]: regional application(0), unit type(0), integrated display(0),
+             // dsc(1), band(1), msg 22 (1), ais mode()
+        254, // payload[25]: ais_communication_state (0)
+    });
+
+    ASSERT_EQ(18, parsed.message_id);
+    ASSERT_EQ(0, parsed.repeat_indicator);
+    ASSERT_EQ(710400932, parsed.user_id);
+    ASSERT_NEAR(-48.4964904785, parsed.longitude, 1e-6);
+    ASSERT_NEAR(-25.51416397094, parsed.latitude, 1e-6);
+    ASSERT_EQ(0, parsed.position_accuracy);
+    ASSERT_EQ(0, parsed.raim);
+    ASSERT_EQ(31, parsed.time_stamp);
+    ASSERT_NEAR(375.4878845, parsed.cog, 1e-6);
+    ASSERT_NEAR(0, parsed.sog, 1e-6);
+    ASSERT_EQ(4, parsed.ais_transceiver_information);
+    ASSERT_NEAR(375.4878845, parsed.heading, 1e-6);
+    ASSERT_EQ(0, parsed.regional_application);
+    ASSERT_EQ(0, parsed.regional_application1);
+    ASSERT_EQ(0, parsed.unit_type);
+    ASSERT_EQ(0, parsed.integrated_display);
+    ASSERT_EQ(1, parsed.dsc);
+    ASSERT_EQ(1, parsed.band);
+    ASSERT_EQ(1, parsed.can_handle_msg_22);
+    ASSERT_EQ(0, parsed.ais_mode);
+    ASSERT_EQ(0, parsed.ais_communication_state);
+}
+
+TEST_F(PGNsTest, it_parses_AISClassAStaticAndVoyageRelatedData)
+{
+    auto parsed = parse_message<AISClassAStaticAndVoyageRelatedData>({
+        5, // payload[0]: msg_id (5), repeat_indicator (0)
+        218,
+        63,
+        82,
+        42, // payload[1..4]: user_id (710033370)
+        106,
+        181,
+        150,
+        0, // payload[5..8]: imo number (9876842)
+        80,
+        85,
+        53,
+        54,
+        57,
+        57,
+        64, // payload[9..15]: call sign -> PU5699@
+        83,
+        65,
+        65,
+        77,
+        32,
+        69,
+        76,
+        69,
+        67,
+        84,
+        82,
+        65,
+        65,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64, // payload[16..35]: name -> SAAM ELECTRAA@@@@@@@@
+        52, // payload[36]: type (52)
+        64,
+        1, // payload[37..38]: length (32.0)
+        120,
+        0, // payload[39..40]: beam (12.0)
+        60,
+        0, // payload[41..42]: position reference from starboard (6.0)
+        120,
+        0, // payload[43..44]: position reference from bow (12.0)
+        215,
+        80, // payload[45..46]: eta date (20695)
+        0,
+        204,
+        191,
+        25, // payload[47..50]: eta time (43200.0)
+        78,
+        2, // payload[51..52]: draft (5.9)
+        80,
+        65,
+        82,
+        65,
+        78,
+        65,
+        71,
+        85,
+        65,
+        32,
+        80,
+        82,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64,  // payload[53..72]: destination (PARANAGUA PR)
+        2,   // payload[73]: ais version (2), gnss type (0), dte (0), reserved ()
+        225, // payload[74]: ais transceiver information (1)
+    });
+
+    ASSERT_EQ(5, parsed.message_id);
+    ASSERT_EQ(0, parsed.repeat_indicator);
+    ASSERT_EQ(710033370, parsed.user_id);
+    ASSERT_EQ(9876842, parsed.imo_number);
+    ASSERT_EQ("PU5699@", parsed.callsign);
+    ASSERT_EQ("SAAM ELECTRAA@@@@@@@", parsed.name);
+    ASSERT_EQ(52, parsed.type_of_ship);
+    ASSERT_NEAR(32, parsed.length, 1e-2);
+    ASSERT_NEAR(12, parsed.beam, 1e-2);
+    ASSERT_NEAR(6, parsed.position_reference_from_starboard, 1e-6);
+    ASSERT_NEAR(12, parsed.position_reference_from_bow, 1e-6);
+    ASSERT_EQ(20695, parsed.eta_date);
+    ASSERT_NEAR(43200, parsed.eta_time, 1e-6);
+    ASSERT_NEAR(5.9, parsed.draft, 1e-6);
+    ASSERT_EQ("PARANAGUA PR@@@@@@@@", parsed.destination);
+    ASSERT_EQ(2, parsed.ais_version_indicator);
+    ASSERT_EQ(0, parsed.gnss_type);
+    ASSERT_EQ(0, parsed.dte);
+    ASSERT_EQ(1, parsed.ais_transceiver_information);
+}
+TEST_F(PGNsTest, it_parses_AISClassBExtendedPositionReport)
+{
+    auto parsed = parse_message<AISClassBExtendedPositionReport>({
+        19, // payload[0]: msg_id (19), repeat_indicator (0)
+        218,
+        63,
+        82,
+        42, // payload[1..4]: user_id (710033370)
+        205,
+        5,
+        24,
+        227, // payload[5..8]: longitude -> -48.4964904785
+        0,
+        217,
+        202,
+        240, // payload[9..12]: latitude -> -25.51416397094
+        124, // payload[13]: pos_accuracy(0), raim(0), time_stamp(31)
+        255,
+        255, // payload[14..15]: cog (0)
+        0,
+        0, // payload[16..17]: sog (0)
+        0,
+        0,  // payload[18..19]: unused
+        52, // payload[20]: type (52)
+        255,
+        255, // payload[21..22]: heading (0)
+        16,  // payload[23]: reserved(), gnss type (1)
+        64,
+        1, // payload[24..25]: length (32.0)
+        120,
+        0, // payload[26..27]: beam (12.0)
+        60,
+        0, // payload[28..29]: position reference from starboard (6.0)
+        120,
+        0, // payload[30..31]: position reference from bow (12.0)
+        83,
+        65,
+        65,
+        77,
+        32,
+        69,
+        76,
+        69,
+        67,
+        84,
+        82,
+        65,
+        65,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64,
+        64, // payload[32..51]: name -> SAAM ELECTRAA@@@@@@@@
+        2,
+        225, // payload[52..53]: dte(0), ais mode (1), reserved(), ais transceiver
+             // information (4)
+    });
+
+    ASSERT_EQ(19, parsed.message_id);
+    ASSERT_EQ(0, parsed.repeat_indicator);
+    ASSERT_EQ(710033370, parsed.user_id);
+    ASSERT_NEAR(-48.4964904785, parsed.longitude, 1e-6);
+    ASSERT_NEAR(-25.51416397094, parsed.latitude, 1e-6);
+    ASSERT_EQ(0, parsed.position_accuracy);
+    ASSERT_EQ(0, parsed.ais_raim_flag);
+    ASSERT_EQ(31, parsed.time_stamp);
+    ASSERT_NEAR(375.4878845, parsed.cog, 1e-6);
+    ASSERT_NEAR(0.0, parsed.sog, 1e-6);
+    ASSERT_EQ(52, parsed.type_of_ship);
+    ASSERT_NEAR(375.4878845, parsed.true_heading, 1e-6);
+    ASSERT_EQ(1, parsed.gnss_type);
+    ASSERT_NEAR(32, parsed.length, 1e-2);
+    ASSERT_NEAR(12, parsed.beam, 1e-2);
+    ASSERT_NEAR(6, parsed.position_reference_from_starboard, 1e-6);
+    ASSERT_NEAR(12, parsed.position_reference_from_bow, 1e-6);
+    ASSERT_NEAR(12, parsed.position_reference_from_bow, 1e-6);
+    ASSERT_EQ(0, parsed.dte);
+    ASSERT_EQ(1, parsed.ais_mode);
+    ASSERT_EQ(4, parsed.ais_transceiver_information);
+}
+
+template <typename M>
+M parse_message(vector<uint8_t> const& payload, int size, base::Time const& time)
+{
     Message message;
     message.time = time;
     message.pgn = M::ID;
